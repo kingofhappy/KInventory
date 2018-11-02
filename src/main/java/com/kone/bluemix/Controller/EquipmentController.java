d2e4802f0a2172a6e449e0bc2607e08491d344d4
kingofhappy <kingofhappy@gmail.com>
2018-11-02 17:46:08 +0530

    update

--- src/main/java/com/kone/bluemix/Controller/EquipmentController.java
+++ src/main/java/com/kone/bluemix/Controller/EquipmentController.java
@@ -1,8 +1,11 @@
 package com.kone.bluemix.Controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +13,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import com.kone.bluemix.ExceptionHandling.ErrorCustomResponse;
 import com.kone.bluemix.ModelWrapper.EquipmentWrapper;
 import com.kone.bluemix.ModelWrapper.ResponseWrapper;
+import com.kone.bluemix.ModelWrapper.RowWrapper;
 import com.kone.bluemix.Service.EquipmentService;
+import com.kone.bluemix.Utils.EIConstant;
 
 /**
  * @author sradhakrishnan2
@@ -27,6 +34,10 @@ public class EquipmentController {
 
 	@Autowired
 	private EquipmentService es;
+	
+	@Autowired
+    @Qualifier("simpleRestTemplate")
+    private RestTemplate rt;
 
 	/**
 	   This is control will take to the index page to lauch application
@@ -52,7 +63,6 @@ public class EquipmentController {
 	*/
 	@GetMapping(value = "allequip")
 	public String allEquipments(Model model) {
-
 		List<EquipmentWrapper> resp = es.allEquipments();
 		model.addAttribute("resp", resp);
 		return "allequip";
@@ -65,15 +75,29 @@ public class EquipmentController {
 	/**
 	 * @param eid
 	 * @param model
-	 * @return
+	 * @return 
 	 */
 	@PostMapping(value = "equipid")
 	public String trackEquipment(@RequestParam("id") String eid, Model model) {
-
-		EquipmentWrapper resp = es.trackEquipment(eid);
+		String dURL=EIConstant.CON_URL+EIConstant.DBNAME+EIConstant.DOC+EIConstant.DOC_CHECK+EIConstant.QUOTE+eid+EIConstant.QUOTE;
+	
+		List<RowWrapper> ob=new ArrayList<>(Arrays.asList(es.allDocs(dURL).getBody().getRows()));
+		EquipmentWrapper resp = new EquipmentWrapper();
+		if(!ob.isEmpty() || !ob.equals(null)) {
+		resp = es.trackEquipment(eid);
 		model.addAttribute("resp", resp);
 		return "equipment";
+		}else {
+			ErrorCustomResponse err= new ErrorCustomResponse();
+			err.setDetail(eid);
+			err.setMessage("Equipment doesn't exist. Please add into the Inventory");
+			model.addAttribute("resp", err);
 
+			return "error";
+		}
+		
+		
+		
 	}
 	
 	/**
@@ -99,19 +123,23 @@ public class EquipmentController {
 	@PostMapping(value = "addequip")
 	public String addEquipment(@ModelAttribute("ewForm") EquipmentWrapper ewForm, Model model) {
 		
-		EquipmentWrapper res = es.trackEquipment(ewForm.getId());
-		if(res.getId().equals(null)) {
-		ResponseWrapper resp = es.addEquipment(ewForm);
-		model.addAttribute("resp", resp);
-		model.addAttribute("msg", "Equipment Added Successfully");
-		}
-		else {
-			model.addAttribute("resp", res);
-			model.addAttribute("msg", "Equipment already exit. Please add new equipmenmt");	
-			
-		}
+		String dURL=EIConstant.CON_URL+EIConstant.DBNAME+EIConstant.DOC+EIConstant.DOC_CHECK+EIConstant.QUOTE+ewForm.getId()+EIConstant.QUOTE;
 		
+		RowWrapper[] ob=es.allDocs(dURL).getBody().getRows();
+		ResponseWrapper resp = new ResponseWrapper();
+		if(ob.length==0) {
+			resp = es.addEquipment(ewForm);
+			model.addAttribute("resp", resp);
+			model.addAttribute("msg", "Equipment Added Successfully");		
 		return "success";
+		}else {
+			ErrorCustomResponse err= new ErrorCustomResponse();
+			err.setDetail(ewForm.getId());
+			err.setMessage("Equipment Already exist in the Inventory");
+			model.addAttribute("resp", err);
+
+			return "error";
+		}
 		
 
 	}
